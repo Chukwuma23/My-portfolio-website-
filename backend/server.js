a96681f9506f72937c669138c42dddac83622d77
chukwuma23 <chukwumaisaac23@gmail.com>
2025-11-24 21:11:28 +0100

    Add server file

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'http://localhost:8080',
-  'http://127.0.0.1:8080'
+  'http://127.0.0.1:8080',
+  'https://my-portfolio-message-api-naci.onrender.com'
 ];
 
 app.use(cors({
