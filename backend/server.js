b25eba31ccc88fe7eaf4887b2040d8e37e5b4610
Chukwuma23 <chukwumaisaac23@gmail.com>
2025-11-24 21:53:16 +0100

    Update server.js to trust header

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1); // Trust first proxy
+
 // Enhanced CORS configuration
 const allowedOrigins = [
   'http://localhost:3000',
