c2c9619100e8a30a443d1dd2166e60f4ec506e4a
Chukwuma23 <chukwumaisaac23@gmail.com>
2025-11-24 21:42:41 +0100

    Update server.js again

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:8080',
   'http://127.0.0.1:8080',
-  'https://chukwumakaluwebsiteportfolio.netlify.app/'
+  'https://chukwumakaluwebsiteportfolio.netlify.app'
 ];
 
 app.use(cors({
